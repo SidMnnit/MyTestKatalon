78496984958f42a8e7d3e4693fbd38283fae3959
sid <sid@localhost>
2022-05-04 12:58:24 +0530

    initial push

---
+++ Test Suites/MyTestSUITE.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>MyTestSUITE</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>daa4a89a-392e-461e-8914-06ff212c151d</testSuiteGuid>
+</TestSuiteEntity>
